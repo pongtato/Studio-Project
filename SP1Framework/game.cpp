b2dcb07aa6fe69d5404fa13d890d91a674b664db
pongtato <forjsa@hotmail.sg>
2014-08-18 15:30:14 +0800

    upated spawn

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -118,10 +118,28 @@ void render()
     colour(0x0F);
     cls();
 
-    //render the game
-	
-    //render test screen code (not efficient at all)
-    
+    for ( int i = 1; i<10; ++i)
+	{
+		if ( i%2 == 0)
+		{
+		std::cout << " ";
+		}
+		else
+		{
+			for ( int j = 1; j<12; ++j)
+			{
+				if ( j%2==0)
+				{
+				std::cout << " ";
+				}
+				else 
+				{
+				gotoXY(consoleSize.X/2 - (i+5), consoleSize.Y/4 + j);
+				std::cout << (char)5;
+				}
+			}
+		}
+	}
 
     // render time taken to calculate this frame
     gotoXY(70, 0);
