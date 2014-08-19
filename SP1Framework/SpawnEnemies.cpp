b58adc4cb5b4605f181fde5fa5f5cff8a3216c20
pongtato <forjsa@hotmail.sg>
2014-08-19 15:18:32 +0800

    Remove old enemy spawn

--- SP1Framework/SpawnEnemies.cpp
+++ SP1Framework/SpawnEnemies.cpp
@@ -6,50 +6,7 @@
 
 ENEMY counter[26];
 ENEMY Wew;
-
-void EnemySpawn(ENEMY& enemy,std::string name,char icon, COORD coordinates, bool Active, int hp, int score,int number)
-{
-
-	enemy.name = name;
-	enemy.icon = icon;
-	enemy.coordinates.X = coordinates.X;
-	enemy.coordinates.Y = coordinates.Y;
-	enemy.Active = Active;
-	enemy.hp = hp;
-	enemy.score = score;
-	enemy.number = number;
-}
-
-void renderEnemies()
-{
-	for ( int i = 0; i<10; ++i)
-	{
-
-		if ( i%2 == 0)
-		{
-			std::cout << " ";
-		}
-		else
-		{
-			for ( int j = 0; j<12; ++j)
-			{
-				if ( j%2==0)
-				{
-					std::cout << " ";
-				}
-				else 
-				{
-					COORD coord;
-					coord.X = 30 + i;
-					coord.Y = 5 + j;
-					EnemySpawn(Wew,"PongPong",(char)5,coord,true,1,10,i + j-1);
-					gotoXY(Wew.coordinates.X,Wew.coordinates.Y);
-					std::cout << Wew.icon;
-				}
-			}
-		}
-	}
-}
+int state = 0;
 
 void moveEnemies()
 {
@@ -62,16 +19,11 @@ void moveEnemies()
 			
 			if ( counter[i].coordinates.Y<=1 )
 			{
-				gotoXY(counter[i].coordinates.X,counter[i].coordinates.Y++);
-				std::cout << counter[i].icon;
+				counter[i].Active = false;
+				
 			}
 		}
 	}
-}
-
-void stateCheck()
-{
-
 }
 
 void enemyMove(unsigned int &currentEnemy)
